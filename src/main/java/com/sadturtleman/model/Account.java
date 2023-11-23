cf34376bc361c35d8533399452dac61852818e65
Sadturtleman <rugsn12345@gmail.com>
2023-11-23 09:38:04 +0900

    JDBC 구현

--- src/main/java/com/sadturtleman/model/Account.java
+++ src/main/java/com/sadturtleman/model/Account.java
@@ -1,21 +1,38 @@
 package com.sadturtleman.model;
 
+import java.util.List;
+
 public class Account {
 
-    private String Id;
+    private String id;
     private String password;
     private String usercode;
 
     public Account(String id, String password){
-        this.Id = id;
+        this.id = id;
         this.password = password;
     }
 
-    public boolean checkAccount(){
-        return true;
+    public boolean isValidAccount(){
+        List<String[]> ls = Jdbc.getinstance().selectAll();
+        
+        for (String[] strings : ls) {
+            if (id.equals(strings[0]) && password.equals(strings[1])){
+                usercode = strings[2];
+                return true;
+            }
+        }
+        
+        return false;
     }
 
+    public void signUp(){
+        Jdbc.getinstance().addValue(id, password);
+    }
+    
     public String getusercode(){
+        if (usercode == null){return "";}
         return usercode;
     }
 }
+ 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module com.sadturtleman {
     requires javafx.controls;    
     requires transitive javafx.graphics;
     requires javafx.fxml;
-
+    requires java.sql;
     opens com.sadturtleman to javafx.fxml;
     opens com.sadturtleman.controller to javafx.fxml;
     exports com.sadturtleman;
